afc7d2d9bb115b7c660ef5700ab7a73a0d9c6047
김수빈 <ksb334ksb@gmail.com>
2022-07-20 17:06:57 +0900

    add : basic eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
-import GlobalStyle from "./styles/globalStyles";
-
-import Counter from "./components/Counter";
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyle from './styles/globalStyles'
+import Main from './pages/Main'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Counter />} />
+          <Route path='/' element={<Main />} />
         </Routes>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from '../components/Counter'
+
+function Main() {
+  const a = 1
+
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+export default Main
